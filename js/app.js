dda2d7c5c7f8e7f341968ad3a2ddbce0bb6f2a84
MD Razikul Islam Joni <mdrazikulislamjoni@gmail.com>
2023-04-02 17:11:07 +0600

    showing modal for every card

--- js/app.js
+++ js/app.js
@@ -5,6 +5,18 @@ const loadAiData = () => {
         .then((data) => diaplayAiData(data.data.tools));
 };
 
+const loadAiDetailsById = (id) => {
+    if (id <= 9) {
+        id = `0${id}`;
+    }
+    console.log(id);
+    const url = `https://openapi.programming-hero.com/api/ai/tool/${id}`;
+    console.log(url)
+    fetch(url)
+        .then((res) => res.json())
+        .then((data) => showAiDetailsModal(data.data));
+};
+
 const diaplayAiData = (ais) => {
     const aiContainer = document.getElementById("ai__container");
     ais.forEach((ai) => {
@@ -30,7 +42,7 @@ const diaplayAiData = (ais) => {
                                     🗓
                                     ${ai.published_in}</div
                                 ></div>
-                                <div style="color: blue; font-size: 46px; cursor: pointer;"> ➡</div>
+                                <div onclick="loadAiDetailsById(${ai.id})" data-bs-toggle="modal" data-bs-target="#aiDetails" style="color: blue; font-size: 46px; cursor: pointer;"> ➡</div>
                             </div>
             </div>
                         
@@ -40,6 +52,44 @@ const diaplayAiData = (ais) => {
     });
 };
 
+// Show AI Details
+const showAiDetailsModal = (details) => {
+    console.log(details);
+    const detailsContainer = document.getElementById("modal__content");
+    detailsContainer.innerHTML = "";
+    const cardContainer = document.createElement("div");
+    cardContainer.classList.add("d-flex");
+    cardContainer.innerHTML = `
+        <div style="background-color: #fef7f7" class="card p-3 me-4">
+            <h5 class="card-title">
+                    ${details.description}
+            </h5>
+            <div class="d-flex" >
+                <div style="background-color: white" class="p-3 fw-bold m-1"> ${details?.pricing[0]?.price} ${details?.pricing[0]?.plan}</div>
+                <div style="background-color: white" class="p-3 fw-bold m-1"> ${details?.pricing[1]?.price} ${details?.pricing[1]?.plan}</div>
+                <div style="background-color: white" class="p-3 fw-bold m-1"> ${details?.pricing[2]?.price} ${details?.pricing[2]?.plan}</div>
+            </div>
+        </div>
+
+        <div class="card p-3">
+            <img
+                src="${details.image_link[0]}"
+                class="card-img-top"
+                alt="..."
+            />
+            <div class="card-body text-center">
+                <h5 class="card-title">
+                    ${details.tool_name}
+                </h5>
+                <p class="card-text">
+                    ${details.description}
+                </p>
+            </div>
+        </div>
+        
+    `;
+    detailsContainer.appendChild(cardContainer);
+};
 const loadFeatures = (features) => {
     features.forEach((feature) => {
         console.log(feature);
